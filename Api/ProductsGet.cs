1050b25c1b65cc5dbf534f51e28db13adc5724e1
Lilian Gassmann <lilian.gassmann@outlook.fr>
2022-01-14 12:20:22 +0100

    Corrected ProductGet result Data Type

--- Api/ProductsGet.cs
+++ Api/ProductsGet.cs
@@ -1,12 +1,8 @@
-using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace Api
 {
@@ -22,7 +18,7 @@ namespace Api
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "products")] HttpRequest req)
         {
-            var products = (ProductData) await productData.GetProducts();
+            var products =  await productData.GetProducts();
             return new OkObjectResult(products);
         }
     }
